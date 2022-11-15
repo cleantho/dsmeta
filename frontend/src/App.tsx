ce38ad8280689db7dd94315bd945e5b0102da335
cleantho <cleantho@gmail.com>
2022-11-15 17:34:03 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,13 @@
+import PersonButton from "./components/personButton"
+
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <PersonButton />
+      <PersonButton />
+      <PersonButton />
+    </>
   )
 }
 
---
+++ frontend/src/components/personButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/person-icon.svg'
+import './styles.css'
+
+function PersonButton() {
+    return (
+        <>
+            <div className="bt-red">
+                <img src={icon} alt="Notificar" />
+            </div>
+        </>
+    )
+}
+
+export default PersonButton
